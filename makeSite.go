aa4ae9c3654e97478f93ac9aec6663caf8d60c34
nordmanii <thomas.nordmann@itslearning.com>
2020-04-13 22:19:41 +0200

    debug

--- makeSite.go
+++ makeSite.go
@@ -2,30 +2,38 @@ package itswizard_siteSetup
 
 import (
 	"github.com/itslearninggermany/itswizard_jwt"
+	"github.com/itslearninggermany/itszwizard_objects"
 )
 
-func (p *SiteSetup) MakeSite(admin bool) (site Site, err error) {
-	if p.newAuth {
-		authToken, user, err := itswizard_jwt.CreateToken(p.r, p.username, p.dbClient, p.dbWebserver)
-		if err != nil {
-			return site, err
+func (p *SiteSetup) MakeSite() (site Site, err error) {
+	if p.site.SessionUser.Username == "" {
+		if p.newAuth {
+			authToken, user, err := itswizard_jwt.CreateToken(p.r, p.username, p.dbClient, p.dbWebserver)
+			if err != nil {
+				return site, err
+			}
+			p.q.Add("key", authToken)
+			p.site.SessionUser = user
+		} else {
+			p.site.SessionUser, err = itswizard_jwt.GetUser(p.r, p.dbWebserver)
+			if err != nil {
+				return p.site, err
+			}
 		}
-		p.q.Add("key", authToken)
-		p.site.SessionUser = user
-	} else {
+	}
+	if !p.newAuth {
 		authtoken, err := itswizard_jwt.ReAuthentificate(p.r, p.dbWebserver, p.dbClient)
 		if err != nil {
 			return site, err
 		}
 		p.q.Add("key", authtoken)
-		p.site.SessionUser, err = itswizard_jwt.GetUser(p.r, p.dbWebserver)
-		if err != nil {
-			return p.site, err
-		}
+
 	}
+
 	p.u.RawQuery = p.q.Encode()
 	p.site.URLQuery = p.u.RawQuery
-	if admin {
+
+	if p.site.SessionUser.Admin {
 		p.site.Navigation = createAdminNavi(p.site.URLQuery)
 	} else {
 		p.site.Navigation = createClientNavi(p.site.URLQuery)
@@ -41,4 +49,19 @@ func (p *SiteSetup) AddSiteName(sitename string) *SiteSetup {
 func (p *SiteSetup) AddDataInSite(data interface{}) *SiteSetup {
 	p.site.Special = data
 	return p
+}
+
+func (p *SiteSetup) GetUser() (user itszwizard_objects.SessionUser, err error) {
+	if p.newAuth {
+		authToken, user, err := itswizard_jwt.CreateToken(p.r, p.username, p.dbClient, p.dbWebserver)
+		if err != nil {
+			return user, err
+		}
+		p.q.Add("key", authToken)
+		p.site.SessionUser = user
+	} else {
+		p.site.SessionUser, err = itswizard_jwt.GetUser(p.r, p.dbWebserver)
+		return p.site.SessionUser, err
+	}
+
 }
--- setUrl.go
+++ setUrl.go
@@ -2,17 +2,17 @@ package itswizard_siteSetup
 
 import "net/url"
 
-func (p *SiteSetup) SetTargetPath (target string) *SiteSetup{
+func (p *SiteSetup) SetTargetPath(target string) *SiteSetup {
 	p.u.Path = target
 	return p
 }
 
-func (p *SiteSetup) AddOpaque (Opaque string) *SiteSetup{
+func (p *SiteSetup) AddOpaque(Opaque string) *SiteSetup {
 	p.u.Opaque = Opaque
 	return p
 }
 
-func (p *SiteSetup) AddUserToUrl (username, password string) *SiteSetup {
+func (p *SiteSetup) AddUserToUrl(username, password string) *SiteSetup {
 	if password == "" {
 		p.u.User = url.User(username)
 	} else {
@@ -21,36 +21,35 @@ func (p *SiteSetup) AddUserToUrl (username, password string) *SiteSetup {
 	return p
 }
 
-func (p *SiteSetup) AddRawPath (rawPath string) *SiteSetup{
+func (p *SiteSetup) AddRawPath(rawPath string) *SiteSetup {
 	p.u.RawPath = rawPath
 	return p
 }
 
-func (p *SiteSetup) SetForceQuery (forceQuery bool) *SiteSetup{
+func (p *SiteSetup) SetForceQuery(forceQuery bool) *SiteSetup {
 	p.u.ForceQuery = forceQuery
 	return p
 }
 
-func (p *SiteSetup) AddRawQuery (rawQuery string) *SiteSetup{
+func (p *SiteSetup) AddRawQuery(rawQuery string) *SiteSetup {
 	p.u.RawQuery = rawQuery
 	return p
 }
 
-func (p *SiteSetup) AddFragment (fragment string) *SiteSetup{
+func (p *SiteSetup) AddFragment(fragment string) *SiteSetup {
 	p.u.Fragment = fragment
 	return p
 }
 
-func (p *SiteSetup) AddValue (value, key string) *SiteSetup{
-	p.q.Add(value,key)
+func (p *SiteSetup) AddValue(value, key string) *SiteSetup {
+	p.q.Add(value, key)
 	return p
 }
 
-func (p *SiteSetup) GetURL (admin bool) (url string, err error) {
-	_, err = p.MakeSite(admin)
+func (p *SiteSetup) GetURL() (url string, err error) {
+	_, err = p.MakeSite()
 	if err != nil {
 		return "", err
 	}
-	 return p.u.String(),nil
+	return p.u.String(), nil
 }
-
